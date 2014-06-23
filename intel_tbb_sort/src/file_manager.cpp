ef638b3315bf00c348c068b65f2e0a3e1afe5c51
novokrest <novokrest013@gmail.com>
2014-06-23 08:59:16 +0400

    change file_manager

--- intel_tbb_sort/src/file_manager.cpp
+++ intel_tbb_sort/src/file_manager.cpp
@@ -1,17 +1,27 @@
 #include "file_manager.hpp"
 #include "thread_pool.hpp"
 #include "utils.hpp"
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
 
 static char const SENTINEL = '\1';
+
+file_manager::file_manager()
+{}
 
 file_manager::file_manager(path const & root_dir)
     : root_dir_(root_dir)
 {}
 
-void file_manager::search()
+void file_manager::set_path(path const & root_dir)
+{
+    root_dir_ = root_dir;
+}
+
+void file_manager::search(suffix_array_builder & sa_builder)
 {
     thread_pool thread_pool(boost::thread::hardware_concurrency());
-    boost::shared_future<void> root_dir_future = thread_pool.add_task(boost::bind(&file_manager::get_files_from_dir, this, root_dir_, boost::ref(thread_pool)));
+    boost::shared_future<void> root_dir_future = thread_pool.add_task(boost::bind(&file_manager::get_files_from_dir, this, root_dir_, boost::ref(thread_pool), boost::ref(sa_builder)));
     remaining_dirs_.push(root_dir_future);
     while(!remaining_dirs_.empty())
     {
@@ -21,37 +31,40 @@ void file_manager::search()
     }
 }
 
-void file_manager::get_files_from_dir(path const & dir, thread_pool & thread_pool)
+void file_manager::get_files_from_dir(path const & dir, thread_pool & thread_pool, suffix_array_builder & sa_builder)
 {
     vpath dir_entries;
     copy(directory_iterator(dir), directory_iterator(), std::back_inserter(dir_entries));
     
-    for (vpath::const_iterator it = dir_entries.begin(); it != dir_entries.end(); ++it) {
+    for (vpath::iterator it = dir_entries.begin(); it != dir_entries.end(); ++it) {
         if (is_symlink(*it)) {
             continue;
         }
 
         if (is_regular_file(*it)) {
-            safe_add_file(*it);
+            safe_add_file(*it, sa_builder);
             continue;
         }
 
         if (is_directory(*it)) {
-            safe_add_visited_dir(*it, thread_pool);
+            safe_add_visited_dir(*it, thread_pool, sa_builder);
         }
     }
 }
 
-void file_manager::safe_add_file(path const & file)
+void file_manager::safe_add_file(path & file, suffix_array_builder & sa_builder)
 {
     boost::lock_guard<boost::mutex> locker(lock_);
-    files_.push_back(file);
+    size_t file_number = files_.size();
+    std::string file_name = file.filename().string();
+    files_.push_back(file.string());
+    sa_builder.add_suffixes(file_name, file_number);
 }
 
-void file_manager::safe_add_visited_dir(path const & dir, thread_pool & thread_pool)
+void file_manager::safe_add_visited_dir(path const & dir, thread_pool & thread_pool,  suffix_array_builder & sa_builder)
 {
     boost::lock_guard<boost::mutex> locker(lock_);
-    boost::shared_future<void> dir_future = thread_pool.add_task(boost::bind(&file_manager::get_files_from_dir, this, dir, boost::ref(thread_pool)));
+    boost::shared_future<void> dir_future = thread_pool.add_task(boost::bind(&file_manager::get_files_from_dir, this, dir, boost::ref(thread_pool), boost::ref(sa_builder)));
     remaining_dirs_.push(dir_future);
 }
 
@@ -60,44 +73,17 @@ bool file_manager::is_found() const
     return !files_.empty();
 }
 
-size_t file_manager::files_count() const
+vstr file_manager::get_files()
 {
-    return files_.size();
+    return std::move(files_);
 }
 
-std::string file_manager::get_filename(size_t index) const
+void file_manager::serialize(binary_oarchive & oar)
 {
-    return files_[index].filename().string();
+    oar << files_;
 }
 
-std::string file_manager::get_full_filename(size_t index) const
+void file_manager::deserialize(binary_iarchive & iar)
 {
-    return files_[index].generic_string();
+    iar >> files_;
 }
-
-std::string file_manager::get_filenames_concatenation() const
-{
-    std::string filenames_concatenation;
-    for (vpath::const_iterator it = files_.begin(); it != files_.end(); ++it) {
-        filenames_concatenation += it->filename().string() + SENTINEL;
-    }
-
-    return std::move(filenames_concatenation);
-}
-
-void file_manager::serialize(std::fstream & output) const 
-{
-    vint offsets(files_.size() + 1);
-    int accum_offset = 0;
-    for (size_t i = 0; i < files_.size(); ++i) {
-        offsets[i] = accum_offset;
-        accum_offset += files_[i].generic_string().length();
-    }
-    offsets[offsets.size() - 1] = accum_offset;
-
-    utils::serialize(files_.size(), output);
-    utils::serialize(offsets, output);
-    utils::serialize(files_, output);
-}
-
-
--- intel_tbb_sort/src/file_manager.hpp
+++ intel_tbb_sort/src/file_manager.hpp
@@ -2,14 +2,21 @@
 #define FILE_MANAGER
 
 #include "thread_pool.hpp"
+#include "suffix_array_builder.hpp"
 #include <boost/filesystem.hpp>
 #include <boost/thread.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
 #include <mutex>
 #include <vector>
 #include <string>
 #include <queue>
+#include <utility>
 
 using namespace boost::filesystem;
+
+using boost::archive::binary_oarchive;
+using boost::archive::binary_iarchive;
 
 typedef std::queue<boost::shared_future<void>> future_queue;
 typedef std::vector<int> vint;
@@ -20,23 +27,25 @@ class file_manager
     boost::mutex lock_;
 
     path root_dir_;
-    vpath files_;
+    vstr files_;
+
     vint filenames_map_;
     future_queue remaining_dirs_;
 
-    void get_files_from_dir(path const & dir, thread_pool & thread_pool);
-    void safe_add_file(path const & file);
-    void safe_add_visited_dir(path const & dir, thread_pool & thread_pool);
+    void get_files_from_dir(path const & dir, thread_pool & thread_pool, suffix_array_builder & sa_builder);
+    void safe_add_file(path & file, suffix_array_builder & sa_builder);
+    void safe_add_visited_dir(path const & dir, thread_pool & thread_pool, suffix_array_builder & sa_builder);
 
 public:
+    file_manager();
     file_manager(path const & root_dir);
-    void search();
+
+    void set_path(path const & root_dir);
+    void search(suffix_array_builder & sa_builder);
     bool is_found() const;
-    size_t files_count() const;
-    std::string get_filename(size_t index) const;
-    std::string get_full_filename(size_t index) const;
-    std::string get_filenames_concatenation() const;
-    void serialize(std::fstream & output) const;
+    vstr get_files();
+    void serialize(binary_oarchive & oar);
+    void deserialize(binary_iarchive & iar);
 };
 
 #endif // FILE_MANAGER
